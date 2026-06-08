3fc789a2b62608d9ceacd0622e237b0f4e17b1b8
bakaapexnv12-sys <bakaapexnv12@gmail.com>
2026-06-08 10:30:14 +0700

    Add success page for successful payment notification

---
+++ success.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="km">
+<head>
+    <meta charset="UTF-8">
+    <title>ជោគជ័យ</title>
+    <style>
+        body { text-align: center; padding-top: 100px; font-family: sans-serif; }
+        .success-box { color: green; }
+    </style>
+</head>
+<body>
+    <div class="success-box">
+        <h1>ការទូទាត់បានជោគជ័យ! 🎉</h1>
+        <p>សូមអរគុណសម្រាប់ការទិញទំនិញរបស់អ្នក។</p>
+        <a href="index.php">ត្រលប់ទៅទំព័រដើម</a>
+    </div>
+</body>
+</html>
